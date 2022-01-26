ba0f174ac6df3df1e5c90dd9905ee7d3f9f2fbc8
Thyara <thyaranonato@outlook.com>
2022-01-26 15:53:49 -0300

    Resoluçao requisito 01

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+const { nameValidation, quantityValidation } = require('./services/middlewares/ProductsValidation');
+const Product = require('./controllers/ProductsControler');
+
+const app = express();
+
+app.use(bodyParser.json());
+
 require('dotenv').config();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.route('/products')
+  .get(rescue(Product.getAll))
+  .post(nameValidation, quantityValidation, rescue(Product.create));
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
