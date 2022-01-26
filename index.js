f0f5921162767543a124074b5427d9a32c9577ba
Thyara <thyaranonato@outlook.com>
2022-01-26 18:51:29 -0300

    Resolvendo requisito 02

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 const { nameValidation, quantityValidation, 
   idValidation } = require('./services/middlewares/ProductsValidation');
 const Product = require('./controllers/ProductsControler');
@@ -14,9 +15,18 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.post('/products', nameValidation, quantityValidation, Product.create);
+app.use((err, _req, res, _next) => {
+  res.status(500);
+  res.json({ error: err });
+});
 
-app.get('/products/:id', idValidation, Product.getById);
+app.route('/products')
+.get(rescue(Product.getAll))
+.post(nameValidation, quantityValidation, rescue(Product.create));
+
+app.route('/products/:id')
+  .get(rescue(idValidation), rescue(Product.getById))
+  .put(rescue(Product.getById));
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
