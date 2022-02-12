bedff1e383077d3611da05d0fcb8a10c8f3f9973
Thyara <thyaranonato@outlook.com>
2022-02-12 15:18:55 -0300

    Alteraçoes para projeto pessoal

--- test/unit-coverage.test.js
+++ test/unit-coverage.test.js
@@ -41,31 +41,4 @@ describe('Testes das camadas Model, Service e Controller', () => {
       expect(coverageResults.total.lines.covered).toBeGreaterThanOrEqual(24);
     });
   });
-  
-  describe('9 - Escreva testes para cobrir 40% das camadas da sua aplicação', () => {
-  
-    it('Será validado que cobertura total das linhas dos arquivos nas pastas `models`, `services` e `controllers` é maior ou igual a 40%', async () => {
-      const coverageResults = await readCoverageFile();
-      expect(coverageResults.total.lines.pct).toBeGreaterThanOrEqual(40);
-      expect(coverageResults.total.lines.covered).toBeGreaterThanOrEqual(24);
-    });
-  });
-  
-  describe('13 - Escreva testes para cobrir 50% das camadas da sua aplicação', () => {
-  
-    it('Será validado que cobertura total das linhas dos arquivos nas pastas `models`, `services` e `controllers` é maior ou igual a 50%', async () => {
-      const coverageResults = await readCoverageFile();
-      expect(coverageResults.total.lines.pct).toBeGreaterThanOrEqual(50);
-      expect(coverageResults.total.lines.covered).toBeGreaterThanOrEqual(24);
-    });
-  });
-
-  describe('14 - Escreva testes para cobrir 60% das camadas da sua aplicação', () => {
-  
-    it('Será validado que cobertura total das linhas dos arquivos nas pastas `models`, `services` e `controllers` é maior ou igual a 60%', async () => {
-      const coverageResults = await readCoverageFile();
-      expect(coverageResults.total.lines.pct).toBeGreaterThanOrEqual(60);
-      expect(coverageResults.total.lines.covered).toBeGreaterThanOrEqual(24);
-    });
-  });
 })
